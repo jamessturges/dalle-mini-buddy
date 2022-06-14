8a69ff5e9b206a3ec67434d297be493b079dde29
James Sturges <66442419+jamessturges@users.noreply.github.com>
2022-06-14 16:55:27 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+import spinner from './spinner.svg'
 import './App.css';
+import { useDallEMini } from './use-dall-e-mini';
 
 function App() {
+
+  const [prompt, setPrompt] = useState("")
+  const [getDallEMiniImages, imageStrings, loading, attempts] = useDallEMini()
+
+  const handleSubmit = (event: React.MouseEvent<HTMLButtonElement>) => {
+
+    event.preventDefault();
+
+    if(prompt !== "") {
+      getDallEMiniImages(prompt);
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Header">
+        <h1>DALL-E Mini Buddy</h1>
+        <h2>Auto-retries! No more traffic issues!</h2>
       </header>
+
+      <body>
+        <form className="Prompt">
+          Enter a prompt:
+          <input className="prompt" onChange={e => setPrompt(e.target.value)} />
+          <button className="submit" onClick={handleSubmit} disabled={loading || prompt === ""}>Generate Images</button>
+        </form>
+
+        {loading && 
+        <>
+          <img src={spinner} alt="Loading ..." />
+          <br />
+          Attempts: {attempts}
+        </>}
+
+        {imageStrings.length > 0 &&
+        <div className='Images' >
+          {imageStrings.map((imageString, index) => <img src={"data:image/png;base64, " + imageString} alt={index.toString()}/>)}
+        </div>}
+      </body>
+
+      <footer className='Footer'>
+        <a href="https://huggingface.co/spaces/dalle-mini/dalle-mini" target="_blank" rel="noreferrer noopener">Original DALL-E Mini website</a><br />
+        If this is causing trouble, <a href="mailto:jamessturgesiii@gmail.com">email me</a>
+      </footer>
     </div>
   );
 }
---
+++ src/use-dall-e-mini.tsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+
+export function useDallEMini(): [(prompt: string) => void, string[], boolean, number] {
+
+    const initialValue: string[] = [];
+    const [imageStrings, setImageStrings] = useState(initialValue);
+    const [loading, setLoading] =  useState(false);
+    const [attempts, setAttempts] = useState(1);
+
+    function sleep(ms: number) {
+
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    function getDallEMiniImages (prompt: string) {
+
+        setLoading(true);
+        const repsonse = keepTrying(prompt);
+
+        repsonse.then(imageStrings => setImageStrings(imageStrings));
+      }
+
+      async function keepTrying(prompt: string) {
+
+        let keepTrying = true;
+        let imageStrings: string[] = [];
+
+        let tryingCount = 1;
+        while (keepTrying) {
+            const resultPromise = await getDallEMiniImagesFromAPI(prompt);
+            if(resultPromise) {
+                imageStrings = resultPromise;
+                keepTrying = false;
+            } 
+            else {
+                setAttempts(tryingCount);
+                await sleep(3000);
+            }
+
+            tryingCount++;
+        }
+        
+        return imageStrings;
+      }
+
+
+      async function getDallEMiniImagesFromAPI(prompt: string) {
+
+        const url = "https://bf.dallemini.ai/generate"
+    
+        try {
+            const response = await fetch(url, {
+                                        method: "POST",
+                                        headers: {
+                                            'Access-Control-Allow-Origin' : '*',
+                                            'Access-Control-Allow-Methods':'POST,OPTIONS',
+                                            "User-Agent": "request",
+                                            "Content-Type": "application/json"
+
+                                        },
+                                        body: JSON.stringify({
+                                            "prompt": prompt
+                                        })
+                                });
+            if(response.status === 200) {
+                const responseJSON: any = await response.json();
+                const imageStrings: string[] = responseJSON.images;
+            
+                setLoading(false);
+                return imageStrings;
+            }
+
+        }
+            catch (e) {
+                console.log("error: " + e)
+            }
+      }
+
+      return [getDallEMiniImages, imageStrings, loading, attempts]
+}
